260ae512c5f12754681f48aa335a94a1c8d86ed9
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-07-22 19:29:26 +0400

    feat: complete Stats component

--- components/home/about/Stats.tsx
+++ components/home/about/Stats.tsx
@@ -23,9 +23,6 @@ export const Stats = () => {
             <span className="chip">Postgres</span>
             <span className="chip">MongoDB</span>
             <span className="chip">GitHub</span>
-            <span className="chip">Jira</span>
-            <span className="chip">Heroku</span>
-            <span className="chip">AWS</span>
           </div>
         </div>
       </Reveal>
@@ -36,16 +33,10 @@ export const Stats = () => {
             <span>Use for fun</span>
           </h4>
           <div className={styles.statGrid}>
-            <span className="chip">Rust</span>
             <span className="chip">Tailwind</span>
-            <span className="chip">Java</span>
-            <span className="chip">Spring</span>
             <span className="chip">Figma</span>
-            <span className="chip">Whimsical</span>
-            <span className="chip">Planetscale</span>
-            <span className="chip">GraphQL</span>
-            <span className="chip">Python</span>
-            <span className="chip">FastAPI</span>
+            <span className="chip">Styled Components</span>
+            <span className="chip">Bootstrap</span>
           </div>
         </div>
       </Reveal>
